4c04c116242f5eac3b094baa91f51741ba969dc6
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-20 17:26:47 +0530

    middlewares - (loggerMiddleware)

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import bugsReducer from '../bugs/reducers/bugsReducer';
 import projectsReducer from '../projects/reducers/projectsReducer';
 
@@ -7,6 +7,19 @@ const rootReducer = combineReducers({
     projectsState : projectsReducer
 });
 
-const appStore = createStore(rootReducer);
+function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.log("Before -> ", store.getState());
+            console.log(action);
+            next(action);
+            console.log("After -> ", store.getState());
+            console.groupEnd();
+        }
+    }
+}
+
+const appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default appStore;
